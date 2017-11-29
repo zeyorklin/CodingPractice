198c1a88bfc5944c72f73491e56cb2e9fc5631f9
Tony (Zhe Yu) Lin <tonyzeyorklin@gmail.com>
2017-11-29 16:01:44 -0500

    Linked list practice

--- src/LinkedList_4.java
+++ src/LinkedList_4.java
@@ -16,7 +16,65 @@ public class LinkedList_4 {
 			first = second;
 		}
 		System.out.println(head.printLinkedList());
-		System.out.println("Partition at 10: " + partitionList(head, 10).printLinkedList());
+		System.out.println("Partition at 10: " + partitionListOptimal2(head, 10).printLinkedList());
+	}
+	
+	public static LinkedList partitionListOptimal2(LinkedList n, int x)
+	{
+		LinkedList head = n;
+		LinkedList tail = n;
+		
+		while(n != null)
+		{
+			LinkedList next = n.next;
+			if(n.data < x)
+			{
+				System.out.println(head.printLinkedList());
+				n.next = head;
+				head = n;
+			}
+			else
+			{
+				tail.next = n;
+				tail = n;
+			}
+			
+			n = next;
+		}
+		tail.next = null;
+		
+		return head;
+	}
+	
+	public static LinkedList partitionListOptimal(LinkedList n, int x)
+	{
+		LinkedList smallerList = null;
+		LinkedList biggerList = null;
+		
+		while(n != null)
+		{
+			LinkedList next = n.next;
+			if(n.data < x)
+			{
+				n.next = smallerList;
+				smallerList = n;
+			}
+			else
+			{
+				n.next = biggerList;
+				biggerList = n;
+			}
+			n = next;
+		}
+		
+		if(smallerList == null)
+			return biggerList;
+		
+		LinkedList head = smallerList;
+		while(smallerList.next != null)
+			smallerList = smallerList.next;
+		smallerList.next = biggerList;
+		return head;
 	}
 	
 	public static LinkedList partitionList(LinkedList n, int x)
