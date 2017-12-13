1a88e0c084d5f5699f3db081accc2dac3767d342
Tony (Zhe Yu) Lin <tonyzeyorklin@gmail.com>
2017-12-13 16:44:06 -0500

    stack practice

--- src/stackAndQueuePractice/SetOfStacks.java
+++ src/stackAndQueuePractice/SetOfStacks.java
@@ -3,5 +3,20 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class SetOfStacks {
-
+	ArrayList<Stack> stacks = new ArrayList<Stack>();
+	public int capacity;
+	
+	public SetOfStacks(int capacity)
+	{
+		this.capacity = capacity;
+	}
+	
+	public Stack lastStack()
+	{
+		if(stacks.size() == 0)
+			return null;
+		
+		return stacks.get(stacks.size() - 1);
+	}
+	
 }
