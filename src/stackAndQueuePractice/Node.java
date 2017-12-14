c950b23791453273b74591da1276357887f9dab8
Tony (Zhe Yu) Lin <tonyzeyorklin@gmail.com>
2017-12-13 21:05:16 -0500

    minor changes

---
+++ src/stackAndQueuePractice/Node.java
@@ -0,0 +1,11 @@
+package stackAndQueuePractice;
+
+public class Node {
+	public Node above;
+	public Node below;
+	public int value;
+	public Node(int value)
+	{
+		this.value = value;
+	}
+}
--- src/stackAndQueuePractice/Stack.java
+++ src/stackAndQueuePractice/Stack.java
@@ -1,5 +1,31 @@
 package stackAndQueuePractice;
 
 public class Stack {
-
-}
+	private int capacity;
+	public Node top;
+	public Node bottom;
+	public int size=0;
+	
+	public Stack(int capacity)
+	{
+		this.capacity = capacity;
+	}
+	
+	public boolean isFull()
+	{
+		return size == capacity;
+	}
+	
+	public void join(Node above, Node below)
+	{
+		if(below != null)
+			below.above = above;
+		if(above != null)
+			above.below = below;
+	}
+	
+	public void push()
+	{
+		
+	}
+}	
