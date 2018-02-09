7e0806340a7e63352e7370680ea3ad8dc807348c
Tony (Zhe Yu) Lin <tonyzeyorklin@gmail.com>
2018-02-09 10:03:42 -0500

    Stack practice

--- src/stackAndQueuePractice/StackSorting.java
+++ src/stackAndQueuePractice/StackSorting.java
@@ -4,10 +4,18 @@ import java.util.Stack;
 public class StackSorting {
 	public static void main(String[] args)
 	{
-		
+		Stack<Integer> s = new Stack<Integer>();
+		s.push(5);
+		s.push(4);
+		s.push(10);
+		s.push(8);
+		s.push(11);
+		System.out.println(s.peek());
+		sort(s);
+		System.out.println(s.peek());
 	}
 	
-	void sort(Stack<Integer> s)
+	public static void sort(Stack<Integer> s)
 	{
 		Stack<Integer> r = new Stack<Integer>();
 		while(!s.isEmpty())
@@ -19,6 +27,9 @@ public class StackSorting {
 			}
 			r.push(temp);
 		}
+		
+		while (!r.empty())
+			s.push(r.pop());
 		
 	}
 }
