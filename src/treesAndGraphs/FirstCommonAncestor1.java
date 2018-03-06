70ad405708c3ecca2df8ac72fd94ba3fb805e03f
Tony (Zhe Yu) Lin <tonyzeyorklin@gmail.com>
2018-03-06 09:54:08 -0500

    More tree practice

--- src/treesAndGraphs/FirstCommonAncestor1.java
+++ src/treesAndGraphs/FirstCommonAncestor1.java
@@ -7,14 +7,14 @@ public class FirstCommonAncestor1 {
 		int[] array = {1,2,3,4,5,6,7,8,9,10};
 		TreeNode root = BST_MinHeight.createMinHeightBST(array);
 		TreeNode first = root.find(10);
-		TreeNode second = root.find(4);
-		if(findCommonAncestor(first, second) == null)
+		TreeNode second = root.find(7);
+		if(findCommonAncestor2(root, first, second) == null)
 		{
 			System.out.println("the two nodes have no common ancestor");
 		}
 		else
 		{
-			System.out.println("The commong ancestor for " + first.data + " and " + second.data + " is " + findCommonAncestor(first, second).data);
+			System.out.println("The commong ancestor for " + first.data + " and " + second.data + " is " + findCommonAncestor2(root, first, second).data);
 		}
 	}
 	
@@ -43,5 +43,40 @@ public class FirstCommonAncestor1 {
 			level--;
 		}
 		return node;
+	}
+	
+	public static TreeNode findCommonAncestor2(TreeNode root, TreeNode first, TreeNode second)
+	{
+		if(!searchSubTree(root, first) || !searchSubTree(root, second))
+			return null;
+		else if(searchSubTree(second, first))
+			return second;
+		else if(searchSubTree(first, second))
+			return first;
+		
+		TreeNode sibling = findSibling(first);
+		TreeNode parent = first.parent;
+		while(!searchSubTree(sibling, second))
+		{
+			sibling = findSibling(parent);
+			parent = parent.parent;
+		}
+		return parent;
+	}
+	
+	public static TreeNode findSibling(TreeNode node)
+	{
+		if(node == null || node.parent == null)
+			return null;
+		return node.parent.left == node ? node.parent.right : node.parent.left;
+	}
+	
+	public static boolean searchSubTree(TreeNode root, TreeNode node)
+	{
+		if(root == null)
+			return false;
+		if(root == node)
+			return true;
+		return searchSubTree(root.left, node) || searchSubTree(root.right, node);
 	}
 }
